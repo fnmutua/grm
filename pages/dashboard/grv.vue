0092841f7b2a0157b802e06d59d61803e1857bd7
fnmutua <felix.mutua@gmail.com>
2024-06-16 20:03:58 +0300

    added mapbox dark

--- pages/dashboard/grv.vue
+++ pages/dashboard/grv.vue
@@ -11,9 +11,17 @@
 
     <!-- Main Content -->
     <div class="col-span-1 md:col-span-10 p-4">
-       
-            
-          
+      <MapboxMap
+      map-id="{ID}"
+      style="  width: 80%;"
+      :options="{
+        style: isDark?'mapbox://styles/agspatial/clqcfzcoa00bt01nwhmf465f7': 'mapbox://styles/mapbox/light-v11', // style URL
+
+        center: [39.075219, 0.26921], // starting position
+        zoom: 5 // starting zoom
+      }"
+    />
+           
     </div>
   </div>
    
@@ -26,10 +34,25 @@
  definePageMeta({
    layout: "landing",
  });
- 
-  
-   
-  
+ import { ref, computed, watch } from 'vue';
+
+ const isDarkMode = ref(false);
+ const colorMode = useColorMode()
+
+ const isDark = computed({
+  get () {
+    return colorMode.value === 'dark'
+  },
+  set () {
+    colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
+  }
+})
+
+    // Watch the computed property `isDark`
+    watch(isDark, (newValue, oldValue) => {
+      console.log('isDark changed from', oldValue, 'to', newValue);
+      // Perform any additional actions you need when `isDark` changes
+    });  
  </script>
 
 <style scoped>
