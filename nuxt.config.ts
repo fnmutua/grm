6dbde58a3925081c4d20d269f41b8d3b0f2b7bc2
fnmutua <felix.mutua@gmail.com>
2024-08-04 20:35:26 +0300

    added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: true,
-  sourcemap: false,
+  sourcemap: true,
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
   postcss: {
